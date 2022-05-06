79de1f5f4f13decb8ef662017254f61bee8a71fe
mikeddecker <92545491+mikeddecker@users.noreply.github.com>
2022-05-06 13:04:25 +0200

    Toestel unit test afgewerkt

--- DomeinTest/UnitTestToestel.cs
+++ DomeinTest/UnitTestToestel.cs
@@ -31,5 +31,54 @@ namespace DomeinTest {
             Toestel toestel = new Toestel("fiets", "urls/fiets");
             Assert.Throws<ToestelException>(() => toestel.ZetToestelnaam(toestelnaamnaam));
         }
+
+        [Theory]
+        [InlineData("afbeeldingUrl", "afbeeldingUrl")]
+        [InlineData("afbeeldingUrl     ", "afbeeldingUrl")]
+        [InlineData("afbeeldingUrl ", "afbeeldingUrl")]
+        [InlineData("     afbeeldingUrl", "afbeeldingUrl")]
+        public void ZetAfbeeldingUrl_valid(string urlIn, string urlUit) {
+            // in feite: Path.Exists
+            //Assert.True(Path.Exists(urlIn));
+            // TODO
+            Toestel toestel = new Toestel("Fiets", "images/url1");
+            Assert.Equal("images/url1", toestel.AfbeeldingUrl);
+            toestel.ZetAfbeeldingUrl(urlIn);
+            Assert.Equal(urlUit, toestel.AfbeeldingUrl);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\n")]
+        [InlineData("   \r   ")]
+        [InlineData(null)]
+        public void ZetAfbeeldingUrl_invalid(string url) {
+            Toestel t = new Toestel("fiets", "url");
+            Assert.Equal("url", t.AfbeeldingUrl);
+            Assert.Throws<ToestelException>(() => t.ZetAfbeeldingUrl(url));
+        }
+
+        [Fact]
+        public void Ctor_valid() {
+            Toestel t = new Toestel("loopband", "loopband url");
+            Assert.Equal("loopband", t.Toestelnaam);
+            Assert.Equal("loopband url", t.AfbeeldingUrl);
+        }
+
+        [Theory]
+        [InlineData("", "goede url")]
+        [InlineData(" ", "goede url")]
+        [InlineData("\n", "goede url")]
+        [InlineData("   \r   ", "goede url")]
+        [InlineData(null, "goede url")]
+        [InlineData("loopband", "")]
+        [InlineData("loopband", " ")]
+        [InlineData("loopband", "\n")]
+        [InlineData("loopband", "   \r   ")]
+        [InlineData("loopband", null)]
+        public void Ctor_invalid(string toestelnaam, string url) {
+            Assert.Throws<ToestelException>(() => new Toestel(toestelnaam, url));
+        }
     }
 }
