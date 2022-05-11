656c790a9a813a26b098bec2dcc944bf485dde99
mikeddecker <92545491+mikeddecker@users.noreply.github.com>
2022-05-11 15:40:39 +0200

    ReservatieRepo aangemaakt

---
+++ FitnessReservatieBL/Managers/ReservatieManager.cs
@@ -0,0 +1,18 @@
+﻿using FitnessReservatieBL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessReservatieBL.Managers {
+    public class ReservatieManager {
+        private IReservatieRepository Repository;
+
+        public ReservatieManager(IReservatieRepository repository) {
+            Repository = repository;
+        }
+
+
+    }
+}
